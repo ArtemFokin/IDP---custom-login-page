ea24643ee6704d4de7c88cbc0b502eb676a7427a
Artem Fokin <artem.fokin@exilion.com>
2023-05-08 17:13:31 +0400

    Update styles

--- src/components/GoogleSinIn.tsx
+++ src/components/GoogleSinIn.tsx
@@ -1,5 +1,6 @@
 import { FC, useEffect, useRef } from "react";
 import { GOOGLE_CLIENT_ID } from "../constants";
+import { Portal } from 'react-portal';
 
 export const GoogleSignIn: FC<{ callbackUrl: string }> = ({ callbackUrl }) => {
   const isMountedRef = useRef(false);
@@ -16,12 +17,14 @@ export const GoogleSignIn: FC<{ callbackUrl: string }> = ({ callbackUrl }) => {
 
   return (
     <>
-      <div
-        id="g_id_onload"
-        data-client_id={GOOGLE_CLIENT_ID}
-        data-login_uri={callbackUrl}
-        data-auto_prompt="false"
-      />
+      <Portal>
+        <div
+          id="g_id_onload"
+          data-client_id={GOOGLE_CLIENT_ID}
+          data-login_uri={callbackUrl}
+          data-auto_prompt="false"
+        />
+      </Portal>
       <div
         className="g_id_signin"
         data-type="standard"
