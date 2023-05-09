2fafd44019e41f26ae74704e2835480c01422e47
Artem Fokin <artem.fokin@exilion.com>
2023-05-09 15:42:49 +0400

    CSS

---
+++ src/components/PasswordForm/PasswordForm.tsx
@@ -0,0 +1,65 @@
+import { login } from "../../api/idp";
+import commonStyles from "../../styles/common.module.css";
+import styles from "./styles.module.css";
+import { FC, FormEvent, useState } from "react";
+import SendButton from "../SendButton/SendButton";
+import { IoChevronBackCircleOutline } from "react-icons/io5";
+
+type PasswordFormProps = {
+    email: string;
+    onBack: () => void;
+    returnUrl: string;
+};
+
+export const PasswordForm: FC<PasswordFormProps> = ({
+    email,
+    onBack,
+    returnUrl,
+}) => {
+    const [password, setPassword] = useState("");
+    const [disabled, setDisabled] = useState(false);
+
+    const handleSubmit = async (e: FormEvent) => {
+        e.preventDefault();
+        setDisabled(true);
+
+        try {
+            const response = await login({
+                email,
+                password,
+                returnUrl: returnUrl.toString(),
+            });
+
+            window.location.href = response.returnUrl;
+        } catch (err) {
+            console.log(err);
+        }
+
+        setDisabled(false);
+    };
+    return (
+        <div className={commonStyles.stack}>
+            <div className={styles.headerBox}>
+                <IoChevronBackCircleOutline
+                    onClick={onBack}
+                    className={styles.backBtn}
+                />
+                <p className={commonStyles.text}>
+                    Enter Password for <br />
+                    {email}
+                </p>
+            </div>
+            <form onSubmit={handleSubmit} className={commonStyles.formGrid}>
+                <input
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    placeholder="Enter Password"
+                    className={commonStyles.input}
+                    disabled={disabled}
+                />
+                <SendButton type="submit" disabled={disabled} />
+            </form>
+        </div>
+    );
+};
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,73 +1,83 @@
-import {   useMemo, useRef, useState } from "react";
+import { useMemo, useRef, useState } from "react";
 import { GoogleSignIn } from "../components/GoogleSinIn";
-import { PasswordForm } from "../components/PasswordForm";
-import commonStyles from '../styles/common.module.css';
+import { PasswordForm } from "../components/PasswordForm/PasswordForm";
+import commonStyles from "../styles/common.module.css";
 import { EmailForm } from "../components/EmailForm";
 import { SwitchTransition, CSSTransition } from "react-transition-group";
 
 const getReturnUrl = (): URL | undefined => {
-  try {
-    const searchParams = new URLSearchParams(window.location.search);
-    const returnUrlRaw = searchParams.get("ReturnUrl");
-    if (!returnUrlRaw) throw new Error("ReturnUrl not found");
-    const returnUrl = new URL(returnUrlRaw);
-    return returnUrl;
-  } catch (err) {
-    console.log(err);
-  }
+    try {
+        const searchParams = new URLSearchParams(window.location.search);
+        const returnUrlRaw = searchParams.get("ReturnUrl");
+        if (!returnUrlRaw) throw new Error("ReturnUrl not found");
+        const returnUrl = new URL(returnUrlRaw);
+        return returnUrl;
+    } catch (err) {
+        console.log(err);
+    }
 };
 
 const LoginPage = () => {
-  const [emailForPasswordForm, setEmailForPasswordForm] = useState('');
-  
-  const animRef = useRef<HTMLDivElement | null>(null);
-  
-  const returnUrl = useMemo(getReturnUrl, []);
-  const redirect_uri = returnUrl?.searchParams.get("redirect_uri");
-  
-  const onEmailFormFinish = (email:string, isExist: boolean)=>{
-    if(!redirect_uri) return;
-    if (isExist) {
-      setEmailForPasswordForm(email);
-    } else {
-      const redurectURL = new URL(redirect_uri);
-      redurectURL.searchParams.append("error", "NotFound");
-      redurectURL.searchParams.append("email", email);
-      window.location.href = redurectURL.toString();
+    const [emailForPasswordForm, setEmailForPasswordForm] = useState("");
+
+    const animRef = useRef<HTMLDivElement | null>(null);
+
+    const returnUrl = useMemo(getReturnUrl, []);
+    const redirect_uri = returnUrl?.searchParams.get("redirect_uri");
+
+    const onEmailFormFinish = (email: string, isExist: boolean) => {
+        if (!redirect_uri) return;
+        if (isExist) {
+            setEmailForPasswordForm(email);
+        } else {
+            const redurectURL = new URL(redirect_uri);
+            redurectURL.searchParams.append("error", "NotFound");
+            redurectURL.searchParams.append("email", email);
+            window.location.href = redurectURL.toString();
+        }
+    };
+
+    const onPasswordFormBack = () => {
+        setEmailForPasswordForm("");
+    };
+
+    if (!returnUrl || !redirect_uri) {
+        return <div>ReturnUrl not found</div>;
     }
-  }
 
-  const onPasswordFormBack = ()=>{
-    setEmailForPasswordForm('');
-  }
-
-  if (!returnUrl || !redirect_uri) {
-    return <div>ReturnUrl not found</div>;
-  }
-
-  return (
-    <div>
-      <SwitchTransition mode="out-in">
-          <CSSTransition
-            key={emailForPasswordForm}
-            nodeRef={animRef}
-            addEndListener={(done) => {
-              animRef.current?.addEventListener("transitionend", done, false);
-            }}
-            classNames="fade"
-          >
-            <div ref={animRef}>
-              {emailForPasswordForm ? (
-                  <PasswordForm email={emailForPasswordForm} onBack={onPasswordFormBack} returnUrl={returnUrl.toString()} />
-              ) : <div className={commonStyles.stack}>
-                <GoogleSignIn redirect_uri={redirect_uri} />
-                <EmailForm onFinish={onEmailFormFinish} />
-              </div>}
-            </div>
-          </CSSTransition>
-      </SwitchTransition>
-    </div>
-  );
+    return (
+        <div>
+            <SwitchTransition mode="out-in">
+                <CSSTransition
+                    key={emailForPasswordForm}
+                    nodeRef={animRef}
+                    addEndListener={(done) => {
+                        animRef.current?.addEventListener(
+                            "transitionend",
+                            done,
+                            false
+                        );
+                    }}
+                    classNames="fade"
+                >
+                    <div ref={animRef}>
+                        {emailForPasswordForm ? (
+                            <PasswordForm
+                                email={emailForPasswordForm}
+                                onBack={onPasswordFormBack}
+                                returnUrl={returnUrl.toString()}
+                            />
+                        ) : (
+                            <div className={commonStyles.stack}>
+                                <GoogleSignIn redirect_uri={redirect_uri} />
+                                <EmailForm onFinish={onEmailFormFinish} />
+                            </div>
+                        )}
+                    </div>
+                </CSSTransition>
+            </SwitchTransition>
+        </div>
+    );
 };
 
 export default LoginPage;
