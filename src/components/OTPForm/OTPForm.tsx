3856be9a3c0b255c6e72c207c9403296777b2767
Artem Fokin <artem.fokin@exilion.com>
2023-05-09 20:55:02 +0400

    Fix OTPForm

--- src/components/OTPForm/OTPForm.tsx
+++ src/components/OTPForm/OTPForm.tsx
@@ -1,21 +1,24 @@
-import { FC } from "react";
+import { FC, FormEvent } from "react";
+
 import commonStyles from "../../styles/common.module.scss";
 
-type OTPForm = {
-    onBack: () => void;
-    email: string;
+type OTPFormProps = {
+  onBack: () => void;
+  email: string;
 };
 
-const OTPForm: FC<OTPForm> = () => {
-    const onSubmit = () => {};
+const OTPForm: FC<OTPFormProps> = () => {
+  const onSubmit = (e: FormEvent) => {
+    e.preventDefault();
+  };
 
-    return (
-        <form className={commonStyles.stack}>
-            <input type="text" placeholder="Code" />
-            <input type="password" placeholder="Password" />
-            <input type="password" placeholder="Confirm Password" />
-            <button type="submit">Create Account</button>
-        </form>
-    );
+  return (
+    <form className={commonStyles.stack} onSubmit={onSubmit}>
+      <input type="text" placeholder="Code" />
+      <input type="password" placeholder="Password" />
+      <input type="password" placeholder="Confirm Password" />
+      <button type="submit">Create Account</button>
+    </form>
+  );
 };
 export default OTPForm;
