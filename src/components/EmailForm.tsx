1bd72e3e812083023c5a20fce523a9192a4468a8
Artem Fokin <artem.fokin@exilion.com>
2023-05-11 11:48:01 +0400

    Add email form validation

--- src/components/EmailForm.tsx
+++ src/components/EmailForm.tsx
@@ -1,6 +1,7 @@
-import { FC, FormEvent, useState } from "react";
+import { FC, FormEvent, useEffect, useState } from "react";
 
 import { checkEmailExists } from "../api/idp";
+import { isValidEmail } from "../helpers/validation";
 import commonStyles from "../styles/common.module.scss";
 import Input from "./Input/Input";
 import SendButton from "./SendButton/SendButton";
@@ -11,31 +12,50 @@ type EmailFormProps = {
 
 export const EmailForm: FC<EmailFormProps> = ({ onFinish }) => {
   const [email, setEmail] = useState("");
+  const [emailError, setEmailError] = useState("");
   const [disabled, setDisabled] = useState(false);
+
+  const validate = (emailValue: string) => {
+    if (!isValidEmail(emailValue)) {
+      return "Incorrect Email";
+    }
+  };
+
+  useEffect(() => {
+    setEmailError("");
+  }, [email]);
 
   const onSubmit = async (e: FormEvent) => {
     e.preventDefault();
     setDisabled(true);
     try {
+      const error = validate(email);
+      if (error) {
+        throw new Error(error);
+      }
       const { isExist } = await checkEmailExists(email);
       onFinish(email, isExist);
-    } catch (err) {
-      console.log(err);
+    } catch (err: any) {
+      setEmailError(err.message || "Internal error");
+      console.error(err);
     }
     setDisabled(false);
   };
 
   return (
-    <form onSubmit={onSubmit} className={commonStyles.oneLineForm}>
-      <Input
-        type="text"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        placeholder="Enter Email"
-        disabled={disabled}
-        autoFocus
-      />
-      <SendButton type="submit" disabled={disabled} />
-    </form>
+    <div>
+      <form onSubmit={onSubmit} className={commonStyles.oneLineForm}>
+        <Input
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Enter Email"
+          disabled={disabled}
+          autoFocus
+        />
+        <SendButton type="submit" disabled={disabled} />
+      </form>
+      {emailError && <p className={commonStyles.textError}>{emailError}</p>}
+    </div>
   );
 };
